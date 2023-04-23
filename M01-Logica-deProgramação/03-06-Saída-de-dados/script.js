153af70195042128ed2b75349958e64f961fdbfb
Jefferson Castro <jeffersondiginvice7@gmail.com>
2023-04-23 15:05:01 -0300

    saída de dados e exercício

---
+++ M01-Logica-deProgramação/03-06-Saída-de-dados/script.js
@@ -0,0 +1,22 @@
+console.log("Bom dia!")
+console.log("Boa noite!")
+
+const a = 32;
+const b = 2.34567;
+const c = "Bom dia!";
+const d = true;
+console.log("---------------------------"); 
+console.log(typeof a);
+console.log(typeof b.toFixed(3));
+console.log(typeof c);
+console.log(typeof d);
+console.log("---------------------------");
+console.log(a);
+console.log(b.toFixed(3));
+console.log(c);
+console.log(d);
+console.log("---------------------------");
+const idade = 32;
+const salario = 4560.5;
+const nome = "Maria Silva";
+console.log(`A funcionária ${nome} ganha ${salario} e tem ${idade} anos. `)
